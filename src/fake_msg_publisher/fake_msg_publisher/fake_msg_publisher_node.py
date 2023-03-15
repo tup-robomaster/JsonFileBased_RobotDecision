f493411968179a8cbfd9f5b2e93384c4ae75d189
INIF-FISH <iratecat1@outlook.com>
2023-03-15 13:14:50 +0800

    Update FakePub

--- src/fake_msg_publisher/fake_msg_publisher/fake_msg_publisher_node.py
+++ src/fake_msg_publisher/fake_msg_publisher/fake_msg_publisher_node.py
@@ -5,7 +5,7 @@ from robot_interface.msg import CarHP as CarHPMsg
 from robot_interface.msg import CarPos as CarPosMsg
 from robot_interface.msg import GameInfo as GameInfoMsg
 from robot_interface.msg import Serial as SerialMsg
-class FakePub(Node):
+class Lier(Node):
 
     def __init__(self):
         super().__init__('FakeMsgPub')
@@ -13,24 +13,27 @@ class FakePub(Node):
         self.publisher_CarPosMsg = self.create_publisher(CarPosMsg,'/FakePosMsg', 10)
         self.publisher_GameInfoMsg = self.create_publisher(GameInfoMsg,'/FakeGameInfoMsg',10)
         self.publisher_SerialMsg = self.create_publisher(SerialMsg,'/FakeSerialMsg',10)
-        timer_period = 0.5  # seconds
+        timer_period = 0.1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
     
     def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
+        self.publisher_CarHP.publish(self.carHP_msg)
+        self.publisher_CarPosMsg.publish(self.carPos_msg)
+        self.publisher_GameInfoMsg.publish(self.gameInfo_msg)
+        self.publisher_SerialMsg.publish(self.serial_msg)
+
+    def make_fake(self):
+        self.carHP_msg = CarHPMsg()
+        self.carPos_msg = CarPosMsg()
+        self.gameInfo_msg = GameInfoMsg()
+        self.serial_msg = SerialMsg()
+
 
 def main(args=None):
     rclpy.init(args=args)
-
-    minimal_publisher = FakePub()
-
+    minimal_publisher = Lier()
     rclpy.spin(minimal_publisher)
-
     minimal_publisher.destroy_node()
     rclpy.shutdown()
 
