3ab088f9f151a4690207df2c4a1a7753643f280b
INIF-FISH <iratecat1@outlook.com>
2023-04-14 17:59:30 +0800

    Test pre

--- src/robot_decision/include/robot_decision/configs.h
+++ src/robot_decision/include/robot_decision/configs.h
@@ -1,5 +1,5 @@
-#define WayPointsPATH (char *)"/home/ninefish/nine-fish/JsonFileBased_RobotDecision/src/robot_decision/sample/waypoints.json"
-#define DecisionsPATH (char *)"/home/ninefish/nine-fish/JsonFileBased_RobotDecision/src/robot_decision/sample/decisions.json"
+#define WayPointsPATH (char *)"/home/ninefish/nine-fish/JsonFileBased_RobotDecision/src/robot_decision/jsonFile/waypoints.json"
+#define DecisionsPATH (char *)"/home/ninefish/nine-fish/JsonFileBased_RobotDecision/src/robot_decision/jsonFile/decisions.json"
 #define REAL_WIDTH (float)12.
 #define REAL_HEIGHT (float)8.
 #define GOAL_TIME_THR_SEC (int)2
